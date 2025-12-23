d7b81becf898eef0cb1b5c1daad12f312a49b64b
Ashish Bhardwaj <erashish41@gmail.com>
2025-12-23 19:26:05 +0530

    constants are added with model file

---
+++ hospital/management/constants.py
@@ -0,0 +1,23 @@
+
+GENDER = [
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('O', 'Other'),
+]
+
+STATUS = [
+    ('pending', 'Pending'),
+    ('confirmed', 'Confirmed'),
+    ('cancelled', 'Cancelled'),
+]
+
+PAID = [
+    ('yes', 'Yes'),
+    ('no', 'No'),
+]
+
+ROLE_CHOICES = [
+    ('doctor', 'Doctor'),
+    ('patient', 'Patient'),
+    ('admin', 'Admin'),
+]

cb9a80ef5a8405d4f4d0c5011359ee5eb7b5982d
Ashish Bhardwaj <erashish41@gmail.com>
2025-12-24 19:52:53 +0530

    hospital list added with template

--- hospital/management/views.py
+++ hospital/management/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, UpdateView
+from management.models import (
+    CustomUser, Hospital, Department, Doctor, Patient,
+    Appointment, MedicalRecord, Prescription, Billing
+)
 # Create your views here.
+
+class HospitalListView(ListView):
+    model = Hospital
+    context_object_name = "hospitals"
+    template_name = "management_templates/hospital_list.html"
+    
+    

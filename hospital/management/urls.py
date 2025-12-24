d2d10d2e57a86e91e53601aa20ae2a731131f08c
Ashish Bhardwaj <erashish41@gmail.com>
2025-12-25 00:16:27 +0530

    form are added for create and update of models

--- hospital/management/urls.py
+++ hospital/management/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from management.views import HospitalListView, HospitalUpdateView, HospitalDeleteView
+from management.views import HospitalListView, HospitalDetailView, HospitalDeleteView
 
 urlpatterns = [
     path("hospitals/", HospitalListView.as_view(),name="hospital_list"),
-    path("hospitals/<uuid:pk>", HospitalUpdateView.as_view(),name="hospital_detail"),
+    path("hospitals/<uuid:pk>", HospitalDetailView.as_view(),name="hospital_detail"),
     path("hospitals/<uuid:pk>/delete", HospitalDeleteView.as_view(),name="hospital_delete")
 ]
